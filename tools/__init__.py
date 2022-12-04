80e09e825d640d00abf9f7cedb35ca69f0efd52e
s <23429309+salehahr@users.noreply.github.com>
2022-12-04 13:38:40 +0100

    move tools to top folder, add tools.peek

---
+++ tools/__init__.py
@@ -0,0 +1,30 @@
+def parse_lines(filepath: str) -> list[str]:
+    """
+    Returns a list of the lines contained within the file.
+    """
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
+
+
+def peek(filepath: str, n: int = 1) -> str | list[str]:
+    """
+    Returns the first n lines of the file.
+    """
+    with open(filepath, "r") as file:
+        if n == 1:
+            return file.readline().strip()
+        else:
+            return [file.readline().strip() for _ in range(n)]
+
+
+def print_part(func):
+    """
+    Prints header for current part being solved.
+    """
+
+    def _wrapped(*args, **kwargs):
+        print(f"\nPart {kwargs['part']}")
+        return func(*args, **kwargs)
+
+    return _wrapped
