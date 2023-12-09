8c5a07c45e2f85e94f4f884eec90e52bd7c4e89d
s <23429309+salehahr@users.noreply.github.com>
2023-12-09 13:05:04 +0100

    2023-06 now handles edge cases

--- 2023/06/main.py
+++ 2023/06/main.py
@@ -18,9 +18,10 @@ def parse(line: str, part: int) -> list[int]:
 
 def get_roots(a: int, b: int, c: int):
     discriminant = b**2 - 4 * a * c
-    r1 = (-b + math.sqrt(discriminant)) / (2 * a)
-    r2 = (-b - math.sqrt(discriminant)) / (2 * a)
-    return min(r1, r2), max(r1, r2)
+    r1 = (-b - math.sqrt(discriminant)) / (2 * a)
+    r2 = (-b + math.sqrt(discriminant)) / (2 * a)
+    roots_are_int = all([int(r) == r for r in [r1, r2]])
+    return r1, r2, roots_are_int
 
 
 @print_part
@@ -28,8 +29,8 @@ def solve(filepath: str, part: int = 1):
     time, goal = [parse(line, part=part) for line in parse_lines(filepath)]
     num_ways = 1
     for t, g in zip(time, goal):
-        roots = get_roots(1, -t, g)
-        num_ways *= math.ceil(roots[1] - roots[0])
+        *roots, are_int = get_roots(1, -t, g)
+        num_ways *= math.floor(roots[1]) - math.floor(roots[0]) - are_int
     print(num_ways)
 
 
